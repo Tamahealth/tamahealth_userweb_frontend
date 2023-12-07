50c63b53598383ab51368af4093b1658b09b79fa
Nathe Seas <natheseasn@Nathes-MacBook-Air.local>
2023-12-07 11:10:00 -0300

    leftover push

--- src/components/PrescriptionForm/PatientAndUpload1.jsx
+++ src/components/PrescriptionForm/PatientAndUpload1.jsx
@@ -40,26 +40,31 @@ const PatientAndUpload1 = () => {
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <div className="flex flex-col bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <div className="mx-auto p-4 max-w-4xl">
+      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <div className="mb-4 text-center">
-          <h2 className="text-blue-800 text-2xl mb-4">
-            PATIENT & PRESCRIPTION
+          <h2 className="text-blue-500 font-semibold text-3xl mb-4">
+            Patient Details
           </h2>
+          <p className="text-gray-500 text-sm mb-4">
+            Please enter the details for the patient whose name is on the
+            prescription file.
+          </p>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-6">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 mb-6">
+          {/* First Name */}
           <div className="mb-6 md:mb-0">
             <label
-              className="block text-gray-700 text-sm font-bold mb-2"
               htmlFor="first-name"
+              className="block text-gray-700 text-sm font-bold mb-2"
             >
               First Name
             </label>
             <input
-              className="shadow appearance-none border-b border-gray-300 focus:outline-none focus:border-blue-500 w-full py-2 px-3 leading-tight"
+              className="shadow appearance-none border-b border-gray-300 focus:border-blue-500 w-full py-2 px-3 leading-tight"
               type="text"
               id="first-name"
-              placeholder="Patient's First Name"
+              placeholder="First Name"
               name="PatientFirstName"
               value={formData.PatientFirstName || ""}
               onChange={handleChange}
@@ -71,18 +76,19 @@ const PatientAndUpload1 = () => {
               </p>
             )}
           </div>
+          {/* Last Name */}
           <div className="mb-6 md:mb-0">
             <label
-              className="block text-gray-700 text-sm font-bold mb-2"
               htmlFor="last-name"
+              className="block text-gray-700 text-sm font-bold mb-2"
             >
               Last Name
             </label>
             <input
-              className="shadow appearance-none border-b border-gray-300 focus:outline-none focus:border-blue-500 w-full py-2 px-3 leading-tight"
+              className="shadow appearance-none border-b border-gray-300 focus:border-blue-500 w-full py-2 px-3 leading-tight"
               type="text"
               id="last-name"
-              placeholder="Patient's Last Name"
+              placeholder="Last Name"
               name="PatientLastName"
               value={formData.PatientLastName || ""}
               onChange={handleChange}
@@ -183,16 +189,17 @@ const PatientAndUpload1 = () => {
             </div>
           </div>
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mb-6">
-          <div className="mb-6 lg:mb-0 lg:col-span-2">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-6">
+          {/* Patient Address */}
+          <div className="mb-6 md:mb-0 md:col-span-1">
             <label
-              className="block text-gray-700 text-sm font-bold mb-2"
               htmlFor="address"
+              className="block text-gray-700 text-sm font-bold mb-2"
             >
               Patient Address
             </label>
             <input
-              className="shadow appearance-none border-b border-gray-300 focus:outline-none focus:border-blue-500 w-full py-2 px-3 leading-tight"
+              className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type="text"
               id="address"
               placeholder="Street Address"
@@ -256,37 +263,38 @@ const PatientAndUpload1 = () => {
         </div>
 
         <div className="flex flex-col md:flex-row justify-between items-end mb-6">
-          <div className="flex-1 max-w-md">
-            {" "}
+          {/* Cellular input */}
+          <div className="mb-6">
             <label
+              htmlFor="cellular"
               className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="phone"
             >
               Cellular
             </label>
-            <input
-              className="shadow appearance-none border-b border-gray-300 focus:outline-none focus:border-blue-500 w-full py-2 px-3 leading-tight"
-              type="tel"
-              id="cellular"
-              placeholder="Phone Number"
-              autoComplete="on"
-              htmlFor="tel"
-              name="PatientCellular"
-              value={formData.PatientCellular || ""}
-              onChange={handleChange}
-            />
-            {inputError.PatientCellular && (
-              <p className="text-red-500 text-xs italic">
-                {inputError.PatientCellular}
-              </p>
-            )}
+            <div className="flex space-x-3">
+              <input
+                className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline flex-grow"
+                type="tel"
+                id="cellular"
+                placeholder="+1234567890"
+                name="PatientCellular"
+                value={formData.PatientCellular || ""}
+                onChange={handleChange}
+                pattern="\+[0-9]{1,15}"
+              />
+              {inputError.PatientCellular && (
+                <p className="text-red-500 text-xs italic">
+                  {inputError.PatientCellular}
+                </p>
+              )}
+            </div>
           </div>
         </div>
-        <div className="md:self-end">
+        <div className="flex justify-end mt-4">
           <button
-            className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-            type="button"
             onClick={handleNextClick}
+            className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded w-full md:w-auto"
+            type="button"
           >
             Next
           </button>
